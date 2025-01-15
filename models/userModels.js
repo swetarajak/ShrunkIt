29f945a17e1926dc19167da20666442908b34b95
swetarajak <swetarajak001@gmail.com>
2025-01-15 17:41:13 +0530

    User Model Created

---
+++ models/userModels.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+},{ timestamps: true });
+
+UserSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
